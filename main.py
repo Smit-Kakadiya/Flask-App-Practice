6587ec4de9ee24cf807055e32e6cfc67f84547f3
Smit Kakadiya <50748959+SmitKakadiyaa@users.noreply.github.com>
2020-06-30 14:02:26 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template
+# import sll
+
+app = application = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+	
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+	
+@app.route("/login/")
+def login():
+    return render_template("flashlogin.html")
+
+if __name__ == "__main__":
+    # BIND to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
